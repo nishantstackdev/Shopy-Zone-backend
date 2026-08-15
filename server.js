4e7b999c3186d135107868f2b48c1cfeb0a9f8ed
Nishant Mathur <nishantmathur572@gmail.com>
2026-08-15 11:30:04 +0530

    fix cors for vercel frontend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const helmet = require("helmet")
 
 const categoryrouter = require('./routers/Categoryrouter')
 const { BrandRouter } = require('./routers/Brandrouter')
